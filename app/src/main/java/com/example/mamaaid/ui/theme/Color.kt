dec8a71d1a0aa5f3b06c1ab61d6a8d262736f162
Siprizer <AaronKiyingi@gmail.com>
2025-08-04 15:46:38 -0400

    Added Copyright text below files

--- app/src/main/java/com/example/mamaaid/ui/theme/Color.kt
+++ app/src/main/java/com/example/mamaaid/ui/theme/Color.kt
@@ -13,3 +13,22 @@ val Pink40 = Color(0xFF7D5260)
 val AppBackground = Color(0xFF5BBCFF)
 val BlackText = Color(0xFF000000)
 val BottomNavBackground = Color(0xFFFFFAB7)
+
+
+/*
+*   Copyright 2025 Clarke Kiyingi
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
+*
+* */
